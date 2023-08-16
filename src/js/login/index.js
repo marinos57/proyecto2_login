9d7a757cf858a8784ea2dcc62df8753a0fc33810
marino5710 <brianmarin4@gmail.com>
2023-08-16 08:30:56 -0600

    se termin[o de crear el index js

--- src/js/login/index.js
+++ src/js/login/index.js
@@ -1,16 +1,40 @@
-import { validarFormulario, Toast} from "../funciones";
+import { validarFormulario, Toast } from "../funciones";
 
 const formLogin = document.querySelector('form')
 
 const login = async e => {
     e.preventDefault();
 
-    if(!validarFormulario(formLogin)){
+    if (!validarFormulario(formLogin)) {
         Toast.fire({
             icon: 'info',
-            title:'Rellene todos los campos'
+            title: 'Rellene todos los campos'
         })
         return;
     }
-    
+
+    try {
+        const url = '/proyecto2_login/API/login'
+
+        const body = new FormData(formLogin);
+
+        const headers = new Headers();
+
+        headers.append("X-Requested-With", "fetch");
+
+        const config = {
+            method: 'POST',
+            headers,
+            body
+        }
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log(data);
+
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+
+formLogin.addEventListener('submit', login);
